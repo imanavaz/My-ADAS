1f689838814bc15e53a04a95ef225842a04c6067
Iman <i_avazpour@yahoo.com>
2019-05-09 11:53:01 +1000

    Working on bluetooth car door control

--- bdcontrol.py
+++ bdcontrol.py
@@ -7,30 +7,63 @@ import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-DoorLED = 18
-GPIO.setup(DoorLED,GPIO.OUT)
-GPIO.output(DoorLED,GPIO.LOW)#set it to low by default
 
+doorActuator = 18
+GPIO.setup(doorActuator,GPIO.OUT)
+GPIO.output(doorActuator,GPIO.LOW)#it has to be on Low, will only activate when locking/unlocking the door
 
-print "Bluetooth car door control"
+doorStatusLED = 23
+GPIO.setup(doorStatusLED,GPIO.OUT)
+GPIO.output(doorStatusLED,GPIO.LOW)#set it to low by default
+
 
 #'Farnoosh','A0:10:81:C5:13:7C'
 devices = [['Iman','34:14:5F:E2:4E:9C'], ['Farnoosh','A0:10:91:C5:13:7C']]
+doorLocked = True 
 
 
 def checkDevices(deviceList):
-	
 	for d in range(len(deviceList)):
 		result = bluetooth.lookup_name(deviceList[d][1], timeout=3)
 		if (result != None):
 			return d
 	return -1
 
+
+
 def checkCarStatus():
 	return "off" #to check OBDII for sensing car's status
 
 
+
+def checkDoorLocked():
+	global doorLocked
+	return doorLocked
+
+
+
+def lockDoors():
+	GPIO.output(doorActuator,GPIO.HIGH)
+	time.sleep(0.5)
+	GPIO.output(doorActuator,GPIO.LOW)
+	GPIO.output(doorStatusLED,GPIO.HIGH)#LED on means doors are locked
+	global doorLocked 
+	doorLocked = True
+	
+	
+
+def openDoors():
+	GPIO.output(doorActuator,GPIO.HIGH)
+	time.sleep(0.5)
+	GPIO.output(doorActuator,GPIO.LOW)
+	GPIO.output(doorStatusLED,GPIO.LOW)#LED off means doors are open
+	global doorLocked
+	doorLocked = False	
+
+
+
 def checkForPhones():
+	print "Bluetooth car door control active"
 	while True:
 		
 		print "Checking " + time.strftime("%a, %d %b %Y %H:%M:%S", time.gmtime())
@@ -38,13 +71,14 @@ def checkForPhones():
 		if (checkCarStatus () == 'on'):
 			print ("car is on - keeping the doors onchanged")
 		else:
+			global devices
 			response = checkDevices(devices)
-			if (response != -1):
-				print ("Open door - " + devices[response][0]+ " is closeby")
-				GPIO.output(DoorLED,GPIO.HIGH)
-			else:
-				print ("Close door - No Phone detected")
-				GPIO.output(DoorLED,GPIO.LOW)
+			if (response != -1 and checkDoorLocked() == True):
+				print ("Opening doors - " + devices[response][0]+ " is closeby")
+				openDoors()
+			elif (response == -1 and checkDoorLocked() == False):
+				print ("Closing doors - No Phone detected")
+				lockDoors()
 
 		time.sleep(1)
 
